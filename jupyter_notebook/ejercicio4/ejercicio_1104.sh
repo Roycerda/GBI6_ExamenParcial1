3eec16d38bb79b71b4e3c52c77257ef8fc960b17
Guillermo Cerda <roy10998@gmail.com>
2022-01-14 10:40:25 -0500

    Create ejercicio4

--- jupyter_notebook/ejercicio4/ejercicio_1104.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Columna:"
-cut -d ',' -f 7 Buzzard2015_data.csv | head -n 1
-echo "# de distinto valor:"
-cut -d ',' -f 7 Buzzard2015_data.csv | tail -n +2 | sort | uniq | wc -l
-echo "Valor min:"
-cut -d ',' -f 7 Buzzard2015_data.csv | tail -n +2 | sort -n | head -n 1
-echo "Valor max:" 
-cut -d ',' -f 7 Buzzard2015_data.csv | tail -n +2 | sort -n | tail -n 1
